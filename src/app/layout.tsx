c3272d7105cd687827a94acdf9651a5d1c285dda
goodnews-code <goodnewscode@gmail.com>
2026-09-16 21:45:25 +0100

    fix(analytics): update GA4 measurement ID to G-SG6TLFW2HZ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
     >
       {/* Google Analytics GA4 */}
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-GS4WVH7SM6"
+        src="https://www.googletagmanager.com/gtag/js?id=G-SG6TLFW2HZ"
         strategy="afterInteractive"
       />
       <Script id="ga-init" strategy="afterInteractive">
@@ -84,7 +84,7 @@ export default function RootLayout({
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-GS4WVH7SM6');
+          gtag('config', 'G-SG6TLFW2HZ');
         `}
       </Script>
 
